7a30f409f88fb292d7a0a7df01bfd62b6aa8b2c7
radha krishna <radha krishna@jyo-sri>
2020-07-25 12:49:35 +0530

    StudentDetails #exam 2nd question

---
+++ LayeredArchitectureDemo/src/main/java/com/capg/demos/dao/TraineeDaoJdbcBasedImpl.java
@@ -0,0 +1,25 @@
+package com.capg.demos.dao;
+
+import com.capg.demos.model.Trainee;
+
+public class TraineeDaoJdbcBasedImpl implements TraineeDao {
+
+	@Override
+	public void addTrainee(Trainee Trainee) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Trainee findTrainee(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deleteTrainee(int id) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
